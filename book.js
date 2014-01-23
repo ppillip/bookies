c9ca68cfb1ad3e3e7ff39ece1ff6f1190bbbf60b
ppillip <ppillip@gmail.com>
2014-01-23 11:11:31 +0900

    create
    첫번째 소스 올리기

---
+++ book.js
@@ -0,0 +1,139 @@
+Users = new Meteor.Collection("users");
+
+/* server */
+if(Meteor.isServer){
+
+    Meteor.startup(function(){
+        console.log('서버 Started~!!');
+    });
+
+    Meteor.publish("users",function(){
+        return Users.find({},{sort:{registDate:-1}});
+    });
+
+}
+
+Meteor.methods({
+    "create_user":function(usr){
+
+        var row = {
+            "userName" : usr.userName,
+            "userEmail" : usr.userEmail,
+            "userPhone" : usr.userPhone,
+            "registDate" : Date.now()
+        };
+        console.log("create mode");
+        if(row.userEmail.trim() === "") return {err:true,msg:"이메일 누락"};
+        var result =  Users.insert(row);
+        return {err:false,_id:result};
+    }
+    ,"update_user":function(usr){
+
+        console.log( this.isSimulation );
+
+        var row = {
+            "userName" : usr.userName,
+            "userEmail" : usr.userEmail,
+            "userPhone" : usr.userPhone
+            /* "registDate" : Date.now() */
+        };
+        console.log("update mode");
+        console.log(row);
+        return Users.update({_id:usr._id},{$set:row});
+    }
+    ,"remove_user":function(userId){
+        console.log("remove_user : " + userId );
+        return Users.remove({_id:userId});
+    }
+    ,"find_userinfo" : function(userId){
+        console.log("find_userinfo : " + userId );
+        return Users.findOne({_id:userId});
+    }
+
+});
+
+
+/* client */
+if(Meteor.isClient){
+
+    Handlebars.registerHelper('timeago',function(time) {
+        return moment(time).from();
+        //return moment(time).format('MMMM Do YYYY, h:mm:ss a');
+    });
+
+    Meteor.autosubscribe(function(){
+        Meteor.subscribe("users");
+    });
+
+    Template.userList.lists = function (){
+        return Users.find({},{sort:{registDate:-1}}); //여기랑 서버쪽이랑 맞춰져야한다.
+    };
+
+    Template.userList.events = {
+        "click button[name=remove]" : function(){
+            userId = $(this).attr("_id");
+            Meteor.call("remove_user",userId,function(err,result){
+               console.log("삭제되었습니다.");
+            });
+        },
+
+        "click button[name=adjust]" : function(){
+            update_user($(this).attr("_id"));
+        },
+
+        "click #userListTable tr[name=regUser] button[name=cancel]" : function(){
+            $("#userListTable tr[name=regUser]").hide();
+            $("#userListTable tr[name=regUser] input[id^=user]").val("");
+        },
+
+        "click #userListTable tr[name=regUser] button[name=apply]" : function(){
+
+            var usr = {};
+
+            $("#userListTable tr[name=regUser] input[type=text]").each(function(idx,obj){
+                console.log();
+                usr[$(obj).attr("name")] = $(obj).val();
+            });
+
+            console.log(usr);
+
+            Meteor.call("create_user",usr,function(err,result){
+                if(result){
+                    $("#userListTable tr[name=regUser] input[id^=user]").val("");
+                    $("tr[name=regUser] input[type=text]").val("");
+                    //$("tr[name=regUser]").hide();
+                }else{
+                    //alert(result);
+                }
+            });
+        },
+        "click #userListTable caption button" : function(){
+            $("#userListTable tr[name=regUser] input[id^=user]").val("");
+            $("#userListTable tr[name=regUser]").toggle();
+            $("#userListTable tr[name=regUser] input[name=userName]").focus();
+        }
+
+    };
+
+    update_user = function(_id){
+
+        var usr = {};
+
+        usr["_id"]=_id;
+
+        $("tr[name="+_id+"] input[type=text]").each(function(idx,obj){
+
+            if($(obj).attr("name"))
+                usr[$(obj).attr("name")] = $(obj).val();
+
+        });
+
+        Meteor.call("update_user",usr,function(err,result){
+            if(err){
+                alert( err.message );
+            }
+        });
+
+    }
+
+}
---
+++ moment.i18n.kr.js
@@ -0,0 +1,46 @@
+moment.lang('kr', {
+    months : "1월 2월 3월 4월 5월 6월 7월 8월 9월 10월 11월 12월".split(" "),
+    monthsShort : "1월 2월 3월 4월 5월 6월 7월 8월 9월 10월 11월 12월".split(" "),
+    weekdays : "일요일 월요일 화요일 수요일 목요일 금요일 토요일".split(" "),
+    weekdaysShort : "일 월 화 수 목 금 토".split(" "),
+    longDateFormat : {
+        L: "YYYY.MM.DD",
+        LL: "YYYY년 MMMM D일",
+        LLL: "YYYY년 MMMM D일 LT",
+        LLLL: "YYYY년 MMMM D일 dddd LT",
+        LT: "A h시 mm분"
+    },
+    meridiem : {
+        AM : '오전',
+        am : '오전',
+        PM : '오후',
+        pm : '오후'
+    },
+    calendar : {
+        lastDay: "어제 LT",
+        lastWeek: "지난주 dddd LT",
+        nextDay: "내일 LT",
+        nextWeek: "dddd LT",
+        sameDay: "오늘 LT",
+        sameElse: "L"
+    },
+    relativeTime : {
+        M: "한달",
+        MM: "%d달",
+        d: "하루",
+        dd: "%d일",
+        future: "%s 후",
+        h: "한시간",
+        hh: "%d시간",
+        m: "일분",
+        mm: "%d분",
+        past: "%s 전",
+        s: "몇초",
+        ss: "%d초",
+        y: "일년",
+        yy: "%d년"
+    },
+    ordinal : function (number) {
+        return (~~ (number % 100 / 10) === 1) ? '일' : '일';
+    }
+});
